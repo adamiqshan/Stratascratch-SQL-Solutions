c94ed5203e0d3e7193beb4abf8a8408f0e9ab592
Adam Iqshan <omai.mohmed9@gmail.com>
2022-06-05 08:55:54 +0000

    ID 09859

---
+++ Medium/Find the first 50% records of the dataset.sql
@@ -0,0 +1,16 @@
+/* 
+Find the first 50% records of the dataset.
+*/
+
+-- https://platform.stratascratch.com/coding/9859-find-the-first-50-records-of-the-dataset?code_type=1
+
+
+-- MySQL, PostgreSQL Solution
+
+select * from worker
+limit (select count(1)/2 from worker)
+
+
+-- MS Server
+
+select top 50 percent rom worker
